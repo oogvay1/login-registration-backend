560aba5a62fbad963fb0678be7dd5d1658708437
Azimbek Alibekov <atalgat592@gmail.com>
2025-08-29 13:06:24 +0500

    deleted all consoles

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -2,13 +2,11 @@ import Post from "../models/posts.model.js";
 
 export default async function getPosts(req, res) {
 
-
     try {
         const userId = req.user._id.toString();
 
         const posts = await Post.find({ userId });
 
-        console.log(posts && posts);
         res.send(posts);
     } catch (err) {
         res.status(404).json({ message: "User id is not defined" });
